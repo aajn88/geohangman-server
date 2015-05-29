83a4695e1e7d71facfe4871e6a4b84a610d666e4
Antonio Jimenez <aajn88@gmail.com>
2015-05-29 00:24:57 -0500

    Added Application main and servlet version updated

---
+++ src/main/java/com/doers/geohangman/config/Application.java
@@ -0,0 +1,17 @@
+package com.doers.geohangman.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author @author <a href="mailto:aajn88@gmail.com">Antonio Jimenez</a>
+ *
+ */
+@SpringBootApplication
+public class Application {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
