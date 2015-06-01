05b731d8bdcb10c2f414dc62d696a1a1422891ee
Antonio Jimenez <aajn88@gmail.com>
2015-06-01 00:21:13 -0500

    Update geohangman.properties location Fix UsersController

--- src/main/java/com/doers/geohangman/controllers/UsersController.java
+++ src/main/java/com/doers/geohangman/controllers/UsersController.java
@@ -2,10 +2,10 @@ package com.doers.geohangman.controllers;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.doers.geohangman.model.CreateUpdateUserRequest;
@@ -23,12 +23,22 @@ public class UsersController {
 	/** Logger **/
 	private static final Logger LOGGER = LoggerFactory.getLogger(UsersController.class);
 	
+	/**
+	 * This GET method requests a User given his {@code id}
+	 * 
+	 * @param id the User Id
+	 * @return the User or null if he does not exist
+	 */
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
-	public String getUser(@RequestParam(value = "id", required = true) String id) {
+	public String getUser(@PathVariable String id) {
 		LOGGER.debug("Get user: " + id);
 		return id;
 	}
 	
+	/**
+	 * This POST method creates a new User
+	 * @param request
+	 */
 	@RequestMapping(method = RequestMethod.POST)
 	public void create(@RequestBody CreateUpdateUserRequest request) {
 		LOGGER.debug("Creating user: " + request.getUser());
